9155675c0d5178689a35379bcf31ff0fa917c67a
Heba Ali <hshegy2002@gmail.com>
2020-07-22 10:59:06 +0300

    Initial Commit

---
+++ controlBluetooth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  controlBluetooth
+//
+//  Created by HebaMac on 7/22/20.
+//  Copyright © 2020 HSAAA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
